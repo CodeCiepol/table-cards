92cfd26150fa7472640b848604eb5f282df8fb07
CiepLap <adamulicho@gmail.com>
2024-02-25 21:40:15 +0100

    inital typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import HelloWorld from "./components/HelloWorld";
+// import RenderForm from "./components/RenderForm";
+
+const App = () => {
+  return (
+    <div>
+        hello word
+      {/* <HelloWorld /> */}
+      {/* <RenderForm /> */}
+    </div>
+  );
+};
+
+export default App;
