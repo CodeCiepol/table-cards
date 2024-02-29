e3f11797b6cd0159dd9c72b1cbb13e14b0a2ba60
CiepLap <adamulicho@gmail.com>
2024-02-29 23:42:57 +0100

    grid added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const App = () => {
   return (
-    <div class="text-4xl leading-tight">
-        hello word
+    <div className="text-4xl flex justify-center items-center h-full">
+      <div className="grid grid-cols-4 gap-4">
+        <div className="container">1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
     </div>
   );
 };
