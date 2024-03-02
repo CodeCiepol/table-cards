d58346d9ebee049f7d9768010430f267f3127c3f
CiepLap <adamulicho@gmail.com>
2024-03-02 19:05:17 +0100

    flex init

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="text-4xl flex justify-center items-center h-full">
-      <div className="grid grid-cols-4 gap-4">
-        <div className="container">1</div>
+    <div className="text-4xl flex flex-row justify-center items-center h-full">
+      {/* <div className="grid grid-cols-4 gap-4"> */}
+        <div>1</div>
         <div>2</div>
         <div>3</div>
         <div>4</div>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
