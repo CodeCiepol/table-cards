b2a7500f34d7a25d4848140bbc8ac353e1d8f8b0
Ciepol <adamulicho@gmail.com>
2024-03-05 23:22:57 +0100

    heading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Options from "./components/options";
 import Previev from "./components/preview";
+import Heading from "./components/heading";
 
 const App = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center">
-        <div className="text-3xl text-center border-4 border-solid border-slate-500 rounded-lg bg-white w-96">
-          Create your own table cards
-        </div>
+        <Heading/>
         <div className="grid mt-6 grid-cols-2 justify-center gap-x-5 w-3/4">
           <Options />
           <Previev />
---
+++ src/components/heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="text-3xl text-center border-4 border-solid border-slate-500 rounded-lg bg-white w-96">
+      Create your own table cards
+    </div>
+  );
+};
+
+export default Heading;
