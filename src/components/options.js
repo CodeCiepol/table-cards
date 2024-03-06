db1e6eb767b49418e120338de6157726f52c8603
Ciepol <adamulicho@gmail.com>
2024-03-06 21:34:51 +0100

    WIP size_of_card

--- src/components/options.js
+++ src/components/options.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { useState } from "react";
 
 // const
 const SetSizeOfCard = () => {
+  const [size_of_card, set_size_of_card] = useState({ width: 20, height: 10 });
+
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log(size_of_card);
+  }
+
+  function widthChangeHandler(event) {
+    set_size_of_card((size_of_card) => ({
+      ...size_of_card,
+      width: parseInt(event.target.value),
+    }));
+  }
+  
+  function heightChangeHandler(event) {
+    set_size_of_card((size_of_card) => ({
+      ...size_of_card,
+      height: parseInt(event.target.value),
+    }));
+    console.log("zmieniam!")
+  }
   return (
     <>
       <div className=" border-4 border-solid border-slate-500 rounded-md bg-white">
-        <form>
+        <form onSubmit={submitHandler}>
           <p className="my-1 text-center">Size of table card:</p>
           <div className="flex justify-center">
             <input
@@ -13,8 +35,10 @@ const SetSizeOfCard = () => {
               id="width-input"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-1/3 p-2.5"
               placeholder="x"
+              onChange={widthChangeHandler}
               required
             />
+
             <input
               type="number"
               id="height-input"
@@ -24,7 +48,7 @@ const SetSizeOfCard = () => {
             />
           </div>
           <div className="flex justify-center">
-          <button className="button ">Set size</button>
+            <button className="button">Set size</button>
           </div>
         </form>
       </div>
