58e05955096ae63c7bba4651361aedd6e8c50d7d
CiepLap <adamulicho@gmail.com>
2024-03-17 00:34:29 +0100

    page is centered and cleared before printing

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function checkWidth(margin, width_of_card, width_of_paper) {
   let zmienna = Math.floor((width_of_paper - 2 * margin) / width_of_card);
-  console.log(zmienna);
-  return;
+  console.log("na szerokosc zmiesci sie " + zmienna);
+  return (zmienna);
+}
+
+function checkHeight(margin, height_of_card, height_of_paper) {
+  let zmienna = Math.floor((height_of_paper - 2 * margin) / height_of_card);
+  console.log("na wysokość zmiesci sie " + zmienna);
+  return (zmienna);
+}
+const GenerateCards =(props)=>{
+  return (<>
+  </>)
 }
 
 const Preview = ({ props_size_of_card }) => {
@@ -11,20 +21,20 @@ const Preview = ({ props_size_of_card }) => {
   const width_of_paper = 210;
   const height_of_paper = 297;
   checkWidth(margin, props_size_of_card.width, width_of_paper);
-  console.log("na szerokość zmieszczą się {}");
+  checkHeight(margin,props_size_of_card.height, height_of_paper);
 
   return (
     <>
       <div
         className="paperToPrint"
-        // style={{width:`${width_of_paper}mm`, height:`${height_of_paper}mm`, backgroundColor: "white"}} 
+        style={{width:`${width_of_paper}mm`, height:`${height_of_paper}mm`, backgroundColor: "white"}} 
       >
-        {/* <div style={{ margin: `${margin}mm` }}> */}
+        <div style={{ margin: `${margin}mm` }}>
           {/* <p className="left-0">test</p> */}
-          <div>width: {props_size_of_card.width}</div>
+          <div>width: {props_size_of_card.width}aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
           <div>height: {props_size_of_card.height}</div>
         </div>
-      {/* </div> */}
+      </div>
     </>
   );
 };
